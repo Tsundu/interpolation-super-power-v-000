d8c4fa71f37d8c5a817d0fda278169bcb2aa016e
Daniel Osment <osment.tsundu@gmail.com>
2020-01-06 02:46:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,8 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(input)
-  
+  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  if input == colors
+    puts 
+  end
 end 
