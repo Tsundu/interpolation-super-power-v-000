6815bae09c741c807aa1d09260a602870ed55f82
Daniel Osment <osment.tsundu@gmail.com>
2020-01-06 02:47:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,6 +3,6 @@
 def display_rainbow(input)
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   if input == colors
-    puts "R: #{colors[0]}, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n"
+    puts "R: #{colors[0]}, O: , Y: yellow, G: green, B: blue, I: indigo, V: violet\n"
   end
 end 
